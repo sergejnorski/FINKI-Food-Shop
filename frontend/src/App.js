19fa948cc53d358d354f18ab317470f173dcdd02
Dell <Dell@DESKTOP-NGV8MRK>
2025-02-17 17:59:52 +0100

    update username3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-red-600'>test2</h1>
+      <h1 className='text-red-600'>test3</h1>
     </div>
   );
 }
